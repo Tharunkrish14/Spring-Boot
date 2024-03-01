ec07f9f4f2420357aa85deeb723a10c51fbfce00
Tharun PV <92297163+Tharunkrish14@users.noreply.github.com>
2024-03-01 12:25:42 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+public class HelloWorld {
+	public String helloworld() {
+		return "Hello World";
+	}
+	
+
+}
---
+++ SpbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootApplication.class, args);
+	}
+
+}
